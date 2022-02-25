3f2e3ad66d4a83e5e0ee74b2247a00cf1222aa4a
Brian Knauff <bknauff@users.noreply.github.com>
2022-02-25 18:33:45 -0500

    Added more test for divide, minus, and multiply

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -13,8 +13,38 @@ public class SimpleCalculatorTest {
 	}
 	
 	@Test
+	public void testAddTrue() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertTrue(calc.add(5, 5) == 10);
+	}
+	
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3), 15);
+	}
+	
+	@Test
+	public void testMultiplyByZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 0), 0);
+	}
+	
+	@Test
 	public void testMinus() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
+	}
+	
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(10, 2), 5);
+	}
+	
+	@Test (expected = ArithmeticException.class)
+	public void testDivideByZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(5, 0), 0);
 	}
 }
